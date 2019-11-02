25ba2f9f415eadb2396d655d931b0c0444c2b144
shayan <shayanadc@gmail.com>
2019-11-02 17:19:54 +0330

    add user id in domain

--- database/migrations/2019_11_02_063621_create_domains_table.php
+++ database/migrations/2019_11_02_063621_create_domains_table.php
@@ -16,6 +16,7 @@ class CreateDomainsTable extends Migration
         Schema::create('domains', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->integer('user_id');
             $table->boolean('approved')->default(false);
             $table->timestamps();
         });

9fb00e228be427d8a62d0f825a07097385c2596f
shayan <shayanadc@gmail.com>
2019-11-02 10:23:47 +0330

    add mig and model

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -14,6 +14,7 @@ class FeatureContext extends \Tests\TestCase implements Context
     protected $response;
     protected $route;
     protected $header = [];
+    use \Illuminate\Foundation\Testing\DatabaseMigrations;
     /**
      * Initializes context.
      *

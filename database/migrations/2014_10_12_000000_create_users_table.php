3ae79663861f06f5f0d9a2ce842fd69278ed6868
shayan <shayanadc@gmail.com>
2019-11-02 17:45:26 +0330

    remove email verified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('api_token', 80)->after('password')
                 ->unique()

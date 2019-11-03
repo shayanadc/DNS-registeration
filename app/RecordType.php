08f77ec3a569949df8bc6cdf0b60682958c7d875
shayan <shayanadc@gmail.com>
2019-11-03 18:43:16 +0330

    add get all records with domain query

--- app/RecordType.php
+++ app/RecordType.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class RecordType extends Model
 {
     protected $guarded = ['created_at', 'updated_at'];
-    protected $hidden = ["created_at", "updated_at"];
+    protected $hidden = ["created_at", "updated_at","laravel_through_key"];
 
     public function createNewRecord($array){
         return static::create($array);

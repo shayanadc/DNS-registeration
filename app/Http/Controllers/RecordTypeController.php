8909d1a17a25bb53d0984043fc7a9efda3c90610
shayan <shayanadc@gmail.com>
2019-11-05 11:36:18 +0330

    add fresh model after create

--- app/Http/Controllers/RecordTypeController.php
+++ app/Http/Controllers/RecordTypeController.php
@@ -45,7 +45,7 @@ class RecordTypeController extends Controller
             Domain::findOrFail($request->input('domain_id'));
             $recordType = new RecordType();
             $config = $recordType->createNewRecord($request->toArray());
-            dispatch(new DomainResolverJob($config->fresh()->domain->name, $config->fresh()))->delay(Carbon::now()->addMinutes(1));
+            dispatch(new DomainResolverJob($config->fresh()->domain->name, $config->fresh()))->delay(Carbon::now()->addMinutes(5));
             return response()->json(['content' => $config->content, 'domain_id' => $config->domain_id], 200);
         } catch (QueryException $e) {
             return response()->json(['errors' => [['title' => 'Your Content For This Domain Is Not Unique.']]], 400);
